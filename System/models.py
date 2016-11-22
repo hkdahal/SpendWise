fc039e43009e2555d54bc0eca6f6ad0b87c8a2de
Hari Dahal <hxd1478@rit.edu>
2016-11-21 21:34:47 -0500

    Can register a new Family and record Rent expenditure successfully.

--- System/models.py
+++ System/models.py
@@ -6,11 +6,20 @@ from django.db import models
 class Family(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        unique_together = ('name',)
+
+    def __str__(self):
+        return self.name
+
 
 class Rent(models.Model):
     family = models.ForeignKey(Family)
     value = models.IntegerField()
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return 'Rent; ' + self.title + ' for ' + self.family
 
 
 class Grocery(models.Model):
@@ -18,12 +27,18 @@ class Grocery(models.Model):
     value = models.IntegerField()
     title = models.CharField(max_length=200)
 
+    def __str__(self):
+        return 'Grocery: ' + self.title + ' for ' + self.family
+
 
 class Car(models.Model):
     family = models.ForeignKey(Family)
     title = models.CharField(max_length=200)
     insurance = models.IntegerField()
     finance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return 'Car:' + self.title + ' for ' + self.family
 
 
 
--- System/views.py
+++ System/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from . import forms
 
 
 def index(request):
-    return render(request, 'System/index.html')
+    return family_setup(request)
+
+
+def family_setup(request):
+
+    if request.method == 'POST':
+
+        family_form = forms.FamilyForm(data=request.POST)
+        if family_form.is_valid():
+            family = family_form.save()
+            return success(request, what='Family' + family.name)
+        else:
+            return HttpResponse('Invalid Family created')
+    else:
+        family_form = forms.FamilyForm()
+
+    return render(request, 'System/index.html',
+                  context={'given_form': family_form,
+                           'action': 'Family'})
+
+
+def rent_setup(request):
+
+    if request.method == 'POST':
+
+        rent_form = forms.RentForm(data=request.POST)
+        if rent_form.is_valid():
+            rent = rent_form.save()
+            return success(request, what='Rent: '+rent.title)
+    else:
+        rent_form = forms.RentForm()
+
+    return render(request, 'System/index.html',
+                  context={'given_form': rent_form,
+                           'action': 'Rent'
+                           })
+
+
+def success(request, what='Something'):
+    return render(request, 'System/success.html', context={'act': what})
 
